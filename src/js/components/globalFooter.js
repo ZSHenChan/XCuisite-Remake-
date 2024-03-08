33b927277a43fbfee137727981241cb2f0349104
ZSHenChan <zishenchan@gmail.com>
2024-03-08 21:28:28 +0800

    redesign footer

--- src/js/components/globalFooter.js
+++ src/js/components/globalFooter.js
@@ -3,17 +3,12 @@ class GlobalFooter {
 
   addHandlerExpandSection = function () {
     this._parentEl.addEventListener('click', function (e) {
-      e.preventDefault();
       if (e.target.classList.contains('footer__expand-btn')) {
         e.target.classList.toggle('rotate');
         const list = e.target.closest('.globalfooter-column-section');
         list.classList.toggle('expand');
       }
-      if (
-        e.target.classList.contains(
-          'globalfooter-column-section-title-text-button'
-        )
-      ) {
+      if (e.target.classList.contains('globalfooter-column-section-button')) {
         e.target
           .querySelector('.footer__expand-btn')
           .classList.toggle('rotate');
